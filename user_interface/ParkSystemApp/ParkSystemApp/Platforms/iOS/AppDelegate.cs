a2af7d2eedb665beaa0a0acc8a1ce3393ff8d326
stopflunky <relativamemtedd@gmail.com>
2024-12-20 17:26:55 +0100

    creazione progetto .NET MAUI

---
+++ user_interface/ParkSystemApp/ParkSystemApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ParkSystemApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
--- user_interface/ParkSystemApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ParkSystemApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
